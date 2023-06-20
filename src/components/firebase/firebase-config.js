590cebe84fa7a6dff6f493e8401dc010d0d4baaf
Neeraj Mani <neerajmani456@gmail.com>
2023-06-20 23:32:13 +0530

    update: firebase auth update

--- src/components/firebase/firebase-config.js
+++ src/components/firebase/firebase-config.js
@@ -12,13 +12,24 @@ import { getStorage } from "firebase/storage";
 //   appId: "1:1076268213796:web:8f07bf2a96b3ea1e3d5ab1"
 // };
 
+//my firebase
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyCCUH34AUvN-IvKSrVejURcZ792XHRvgzs",
+//   authDomain: "reddit-clone-4a6ca.firebaseapp.com",
+//   projectId: "reddit-clone-4a6ca",
+//   storageBucket: "reddit-clone-4a6ca.appspot.com",
+//   messagingSenderId: "575291513087",
+//   appId: "1:575291513087:web:e664a5b0fb1ffc261594ca",
+// };
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCCUH34AUvN-IvKSrVejURcZ792XHRvgzs",
-  authDomain: "reddit-clone-4a6ca.firebaseapp.com",
-  projectId: "reddit-clone-4a6ca",
-  storageBucket: "reddit-clone-4a6ca.appspot.com",
-  messagingSenderId: "575291513087",
-  appId: "1:575291513087:web:e664a5b0fb1ffc261594ca",
+  apiKey: "AIzaSyBSbK9JyjTyaDYs6QhzJyEaLVU49-cpIlM",
+  authDomain: "reddit-clone-7bb54.firebaseapp.com",
+  projectId: "reddit-clone-7bb54",
+  storageBucket: "reddit-clone-7bb54.appspot.com",
+  messagingSenderId: "988772507032",
+  appId: "1:988772507032:web:e9023870ebc587f1499fed",
 };
 
 // Initialize Firebase
